6452f4c5630e34b536992e2b79a9492f491cdede
dcwesthead <d.c.westhead@gmail.com>
2016-06-07 22:38:40 +0100

    Initial commit

---
+++ Accounts.Application.UnitTests/Commands/CreateAccountCommandUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Accounts.Application.UnitTests
+{
+    [TestClass]
+    public class CreateAccountCommandUnitTests
+    {
+        private CreateAccountCommand _command;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void WhenIAnAccountIsSubmittedForCreationItIsCreated()
+        {
+
+        }
+    }
+}
